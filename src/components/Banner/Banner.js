a97ebdeb0002949e579811832d231920d29464b3
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-03 16:38:35 -0600

    installed axios, setup configuration with the appropiate endpoints, baseURL and API_KEY and fetched data for the banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "../../axios";
+import requests from "../../Requests";
 
 import "./Banner.css";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   const truncate = (string, n) => {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   };
@@ -11,22 +33,21 @@ function Banner() {
     <header
       className="banner"
       style={{
-        backgroundImage: `url('https://i.imgur.com/e1hLQ2m.png')`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundSize: "cover",
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            " This is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a description",
-            150
-          )}
+          {truncate(`${movie?.overview}`, 150)}
         </h1>
       </div>
 
