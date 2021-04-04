4cd1da80bd060ed5b94edb9ab453181251ca277e
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-03 23:37:46 -0600

    created the loginScreen component to display some text and the button to sign in, included a piece of state to render the signIn component in the future and styled the loginScreen component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomeScreen from "./pages/HomeScreen/HomeScreen";
 
 import "./App.css";
+import LoginScreen from "./pages/LoginScreen/LoginScreen";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
       <Router>
-        <Switch>
-          <Route exact path="/" component={HomeScreen} />
-        </Switch>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+          </Switch>
+        )}
       </Router>
     </div>
   );
