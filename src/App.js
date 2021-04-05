644b0149cf1d7bfc98592ac1dd48f4707d187183
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-05 14:07:01 -0500

    created functions to register and sign in users with the firebase authentication funcitonality, and also persist the user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import HomeScreen from "./pages/HomeScreen/HomeScreen";
 
 import "./App.css";
 import LoginScreen from "./pages/LoginScreen/LoginScreen";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        console.log(userAuth);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA25lj2Lrioj9UieLlqme9GvPmmivi14Uw",
   authDomain: "netflix-build-kcc.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "726039258931",
   appId: "1:726039258931:web:3eb9204b0229d124370046",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firestore = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default firestore;
